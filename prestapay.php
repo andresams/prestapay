fab5888790b79112e31fafadf3a01692433cc2ea
Andresa Martins <andresa.martins@bounteous.com>
2019-03-22 22:29:44 -0300

    Add function and template for hook paymentOptions

--- prestapay.php
+++ prestapay.php
@@ -73,4 +73,53 @@ class PrestaPay extends PaymentModule
     {
         return $this->_html;
     }
+
+    /**
+     * Display this module as a payment option during the checkout
+     *
+     * @param array $params
+     * @return array|void
+     */
+    public function hookPaymentOptions($params)
+    {
+        /*
+         * Verify if this module is active
+         */
+        if (!$this->active) {
+            return;
+        }
+
+        /**
+         * Form action URL. The form data will be sent to the
+         * validation controller when the user finishes
+         * the order process.
+         */
+        $formAction = $this->context->link->getModuleLink($this->name, 'validation', array(), true);
+
+        /**
+         * Assign the url form action to the template var $action
+         */
+        $this->smarty->assign(['action' => $formAction]);
+
+        /**
+         *  Load form template to be displayed in the checkout step
+         */
+        $paymentForm = $this->fetch('module:prestapay/views/templates/hook/payment_options.tpl');
+
+        /**
+         * Create a PaymentOption object containing the necessary data
+         * to display this module in the checkout
+         */
+        $newOption = new PrestaShop\PrestaShop\Core\Payment\PaymentOption;
+        $newOption->setModuleName($this->displayName)
+            ->setCallToActionText($this->displayName)
+            ->setAction($formAction)
+            ->setForm($paymentForm);
+
+        $payment_options = array(
+            $newOption
+        );
+
+        return $payment_options;
+    }
 }
---
+++ views/templates/hook/payment_options.tpl
@@ -0,0 +1,12 @@
+{*
+ * PrestaPay - A Sample Payment Module for PrestaShop 1.7
+ *
+ * Form to be displayed in the payment step
+ *
+ * @author Andresa Martins <contact@andresa.dev>
+ * @license http://opensource.org/licenses/afl-3.0.php
+ *}
+<form method="post" action="{$action}">
+    <P>This is the checkout template of our payment module.</P>
+    <P>We can add payment instructions and form fields to it.</P>
+</form>
